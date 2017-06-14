c6cab3fd5120cd4ee7241015d5cf46a39947dbde
Alexys <alexys.lameda@quadminds.com>
2017-06-14 16:15:16 -0300

    Functions map, filter, forEach, toArray, every done.

---
+++ index.js
@@ -0,0 +1,51 @@
+const {
+    entries,
+    map,
+    filter,
+    forEach,
+    every,
+    toArray
+} = require('./lib/functions');
+const extendedObject = (obj = {}, clone = false) => {
+    const inner = Symbol('inner');
+    const extended = function(){
+        return this[inner];
+    };
+    extended[inner] = (clone) ? JSON.parse(JSON.stringify(obj)) : obj;
+    extended.map = map;
+    extended.filter = filter;
+    extended.forEach = forEach;
+    extended.every = every;
+    extended.toArray = toArray;
+    extended.symbol = () => inner;
+    const protectedProperties = [
+        'map',
+        'filter',
+        'forEach',
+        'every',
+        'toArray',
+        'symbol',
+    ];
+    const proxied = new Proxy(extended, {
+        get: function(target, prop, receiver) {
+            if (protectedProperties.indexOf(prop) >= 0 || prop === inner){
+                return target[prop];
+            }
+            return target[inner][prop];
+        },
+        set: function(target, prop, value, receiver) {
+            if (protectedProperties.indexOf(prop) >= 0){
+               console.error(`Cannot write ${prop}. It's a protected property`);
+               return false;
+            }
+            target[inner][prop] = value;
+            return true;
+        },
+        apply: function(target, thisArg, argumentsList) {
+            return target[inner];
+        }
+    });
+    return proxied;
+};
+
+module.exports = extendedObject;
